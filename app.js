90b5be66b6c426c4302342de2904f1f45039caa8
gialli24 <a.gialli2409@gmail.com>
2026-05-07 15:43:51 +0200

    update orders router renaming it and using it in app.js for /orders routes. remove readme in routers

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const ordersRouter = require('./routers/orders');
+
 app.use(express.static('public'));
+
+app.use('/orders', ordersRouter);
 
 app.get('/', (req, res) => {
     res.send("Hello World");
