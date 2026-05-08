1b2414841b10b3aba3b58dfeec487e84e9f9ce56
gialli24 <a.gialli2409@gmail.com>
2026-05-08 12:46:55 +0200

    update app.js with products router

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const productRoutes = require('./routers/products');
+
+app.use(express.json());
 const cors = require('cors');
 
 const ordersRouter = require('./routers/orders');
 
 app.use(express.static('public'));
 
-/* Body Parser */
-app.use(express.json());
-
-/* Cors middleware */
 app.use(cors());
 
 app.use('/orders', ordersRouter);
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+res.send("Hello World");
 });
 
+app.use('/products', productRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server in ascolto su http://localhost:${PORT}`);
+console.log(`Server in ascolto su http://localhost:${PORT}`);
 });
