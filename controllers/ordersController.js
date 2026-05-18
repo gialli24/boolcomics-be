2bd27a5d857002fa50d5e866929989d37edc3787
gialli24 <a.gialli2409@gmail.com>
2026-05-18 13:54:08 +0200

    fix emails

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -31,7 +31,7 @@ function sendEmail(email, template, typeOfToken, data, orderedProducts) {
                 <div class="product-image">
                     <!-- Placeholder image square -->
                     <div style="width:100px; height:130px; background-color:#f0f0f0; border:1px solid #ddd;">
-                        <img src="${order.image_url}" alt="${order.name}" />
+                        <img src="http://localhost:5173${order.image_url}" alt="${order.name}" />
                     </div>
                 </div>
                 <div class="product-details">
@@ -52,10 +52,14 @@ function sendEmail(email, template, typeOfToken, data, orderedProducts) {
         .replace('{{order_date}}', formatted_date)
         .replace('{{items}}', productsMarkup)
         .replace('{{subtotal}}', Math.floor(productsPrice * 100) / 100)
-        .replace('{{discount_amount}}', discount_amount)
-        .replace('{{discount_percentage}}', discount_percentage + '%')
         .replace('{{total_price}}', Math.floor(total_price * 100) / 100)
         .replace('{{shipping_cost}}', Math.floor(shipping_cost * 100) / 100);
+
+    if (discount_amount > 0) {
+        template = template
+            .replace('{{discount_amount}}', "-" + discount_amount)
+            .replace('{{discount_percentage}}', discount_percentage + '%')
+    }
 
     const client = new MailtrapClient({
         token: typeOfToken
